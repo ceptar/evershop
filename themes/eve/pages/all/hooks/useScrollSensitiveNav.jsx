59abd461eefc168b4f5d0c985cb056d4560ca171
ceptar <christoph.cerjan@gmail.com>
2024-02-23 12:49:18 +0100

    cc

---
+++ themes/eve/pages/all/hooks/useScrollSensitiveNav.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export function useScrollSensitiveNav() {
+  const [navColor, setNavColor] = useState("transparent");
+
+  const listenScrollEvent = () => {
+    window.scrollY < 15  ? setNavColor("transparent") : setNavColor("white");
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenScrollEvent);
+    return () => {
+      window.removeEventListener("scroll", listenScrollEvent);
+    };
+  }, []);
+
+  return { navColor };
+}

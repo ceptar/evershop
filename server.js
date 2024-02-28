8bf91b8d33473054cd759231e46cb7164627eed2
ceptar <christoph.cerjan@gmail.com>
2024-02-28 01:41:11 +0100

    cc

---
+++ server.js
@@ -0,0 +1,7 @@
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.listen(port, '0.0.0.0', () => {
+//     console.log(`Server is running on http://0.0.0.0:${port}`);
+//   });
